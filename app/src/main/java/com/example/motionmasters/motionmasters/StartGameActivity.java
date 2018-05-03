455e83452bb543f92a68e0e9b03c0ba3d7d9c7d2
Daniel Tovesson <daniel.tovesson@me.com>
2018-05-03 11:48:29 +0200

    Code formatting

--- app/src/main/java/com/example/motionmasters/motionmasters/StartGameActivity.java
+++ app/src/main/java/com/example/motionmasters/motionmasters/StartGameActivity.java
@@ -4,23 +4,19 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class StartGameActivity extends AppCompatActivity {
 
     private TextView tv;
-    private Handler handler = new Handler();
     private int switchSec;
-    private int secs;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
-        tv = (TextView) findViewById(R.id.startGameText);
+        tv = findViewById(R.id.startGameText);
         int secs = 7;
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         new CountDownTimer((secs + 1) * 1000, 1000) // Wait 5 secs, tick every 1 sec
